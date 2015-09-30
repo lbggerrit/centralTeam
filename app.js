cbf4e740e223f5d835fae91aaecba9affb79ac3d
Kevin Chapman <kchapman@sapient.com>
2015-09-30 15:24:12 +0100

    renamed older isomorphic server files folder + removed code from app.js
    Change-Id: Id4452af50501b90ec2890fd27b3b960f04ac8708

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-/* globals global */
-global.frameworkGlobals = {
-	isServer: true,
-	isClient: false,
-	isDev: false,
-	basePath: '',
-	fluxForm: {
-		clearOnScript: false
-	}
-};
 
-require('babel/register')({
-	ignore: ['**/jquery.custom*', 'node_modules']
-});
-// require('node-jsx').install({harmony: true});
-require('./hapi-server/server.js');
