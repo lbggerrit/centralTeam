df616c1dee85ba4be042b7bccd854d277c75c377
Duc Hoang <dhoang@sapient.com>
2015-10-01 14:46:33 +0100

    setting a header in the response
    Change-Id: Ifbf8d98a4d2c832f0d25398fa63bd46fd93fbcb4

--- server/rest-services.js
+++ server/rest-services.js
@@ -1,24 +1,26 @@
 var
-	soap = require('soap'),
-	parseString = require('xml2js').parseString,
-	restServices;
+    soap = require('soap'),
+    parseString = require('xml2js').parseString,
+    restServices;
 
 restServices = {
-	getQuote: function(app) {
-		app.get('/get-quote', function(req, res) {
-			var
-				url = 'http://www.webservicex.com/stockquote.asmx?wsdl',
-				args = {symbol: 'AAPL'};
+    getQuote: function (app) {
+        app.get('/get-quote', function (req, res) {
+            var
+                url = 'http://www.webservicex.com/stockquote.asmx?wsdl',
+                args = {symbol: 'AAPL'};
 
-			soap.createClient(url, function(err, Client) {
-				Client.GetQuote(args, function(err, result) {
-					parseString(result[Object.keys(result)[0]], function(err, json) {
-						res.send(json);
-					});
-				});
-			});
-		});
-	}
+            res.setHeader('Access-Control-Allow-Origin', '*');
+
+            soap.createClient(url, function (err, Client) {
+                Client.GetQuote(args, function (err, result) {
+                    parseString(result[Object.keys(result)[0]], function (err, json) {
+                        res.send(json);
+                    });
+                });
+            });
+        });
+    }
 }
 
 module.exports = restServices;
