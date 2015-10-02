8efc053af6e42e80eb9e3d0380ddbd81716362ff
Kevin Chapman <kchapman@sapient.com>
2015-10-02 10:35:13 +0100

    start of a soap to JSON service
    Change-Id: I552cd48a11666a43fe70a1a5ef2fd8991adba882

---
+++ server/soap-to-json.js
@@ -0,0 +1,13 @@
+var
+	soap = require('soap'),
+	parseString = require('xml2js').parseString;
+
+function SoapToJson(url = '', args = {}) {
+	var
+		json;
+
+
+	return json
+}
+
+module.exports = SoapToJson;
