c49d7dc3f248b02ce888e2395668b06f557ad532
James McLeod <jmcleod@sapient.com>
2015-10-04 23:29:36 +0100

    Added sitespeed and pa11y js
    Change-Id: I8e9ae38ec8f9cd54389288d87d2ccd3e603b3eb8

---
+++ gulp-tasks/sitespeed.js
@@ -0,0 +1,8 @@
+module.exports = function (gulp, plugins, config) {
+	return plugins.sitespeed({
+		urls: ['http://localhost:9000/', 'http://localhost:9000/example/', 'http://localhost:9000/example/index.html'],
+		depth: 1,
+    html: 1,
+    resultBaseDir: 'coverage/Sitespeed/'
+	});
+};
